70599e4472cd52da376c3b3e1933c6c88dd01fb7
Adefemi <adefemilouis@gmal.com>
2025-01-27 03:47:02 -0700

    commit

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+declare type SearchParamsProps ={
+    params: { [key: string]: string };
+    searchParams: { [key: string]: string | string[] | undefined }
+};
+
+declare type SignUpParams ={
+    firstName: string;
+    lastName: string;
+    address1: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    dateOfBirth: string; 
+    ssn: string;
+    email: string;
+    password: string;
+
+}
