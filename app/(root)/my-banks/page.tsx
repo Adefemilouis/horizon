45d3a125cd6eb42270e1e1f028aaa6878d478965
Adefemi <adefemilouis@gmal.com>
2025-01-31 14:54:08 -0700

    commit

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mybank = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mybank
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+"use client"
+
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/ui/HeaderBox'
+import RightSideBar from '@/components/ui/RightSideBar';
+
 
 const page = () => {
+  const loggedIn = { firstName: 'Adefemi', lastName: 'Adekunle', email: 'adefemilouis@gmail.com' };
   return (
-    <div>
-      page
-    </div>
+    <>
+       
+
+       <section className="home">
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title="welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Acess and manage your accoumt and transactions efficiently. "
+          />
+           
+            <TotalBalanceBox 
+          accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={[{ currentBalance: 123.50 }, {
+            currentBalance: 500.50
+           }]}
+
+           /> 
+
+        </header>
+
+        RECENT  TRANSACTIONS
+      </div>
+
+      <RightSideBar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{}, {}]}
+      />
+    </section>
+    </>
+    
   )
 }
 
